1df945646428a201d80498bed93c4350ffec5964
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-02-18 11:16:34 +0100

    initial commit

---
+++ horizontal_scroll/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <section>
+      <div className="text_wrapper">
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+        <h1>Tokyo</h1>
+      </div>
+    </section>
+      
+    </>
+  )
+}
+
+export default App
